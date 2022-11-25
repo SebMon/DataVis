130f5328830002d917dfccaaea33756709c7da9a
Mikkel <mikkelpriisholm@gmail.com>
2022-11-25 10:31:16 +0100

    fix

--- CrimeProject/app.R
+++ CrimeProject/app.R
@@ -16,7 +16,9 @@ population = fastmap()
 
 # Data for Calendar diagrams
 #calendar_data <- read.csv('../data/sample.csv')
-calendar_date <- as.Date(data$Start_Date_Time , "%m/%d/%Y")
+#calendar_date <- as.Date(data$Start_Date_Time , "%m/%d/%Y")
+calendar_date <- data$Start_Date_Time_Date_Objects
+calendar_date
 calendar_minDate <- "2017-01-01"
 calendar_maxDate <- "2021-12-31"
 calendar_date <- subset(calendar_date, calendar_date >= calendar_minDate & calendar_date <= calendar_maxDate)
